76ade4d25f20117db83a93353c89a8c989a4c907
陈昱安 <31612534@qq.com>
2020-02-27 01:17:15 +0800

    updata path
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- pros_lvgl_sim_conf.h
+++ pros_lvgl_sim_conf.h
@@ -4,7 +4,7 @@
                             //1 If you want to use a simulator
 
 #if USE_PROS_LVGL_SIM == 1
-#define LVGL_INCLUDE_PATH "pros_lvgl_sim/lvgl/lvgl.h"
+#define LVGL_INCLUDE_PATH "ncrapi_lvgl_sim_kernel/lvgl/lvgl.h"
 #else
 #define LVGL_INCLUDE_PATH "display/lvgl.h"
 #endif
