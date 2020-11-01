ff3d73fbdcb8f666efc044fe762f405219073b07
3038922 <31612534@qq.com>
2020-11-01 20:39:04 +0800

    似乎被玩坏了
    Signed-off-by: 3038922 <31612534@qq.com>

--- src/ncrLvglSimKernel.cpp
+++ src/ncrLvglSimKernel.cpp
@@ -22,9 +22,8 @@ void memory_monitor(lv_task_t *param)
 
     lv_mem_monitor_t mon;
     lv_mem_monitor(&mon);
-    printf("used: %6d (%3d %%), frag: %3d %%, biggest free: %6d\n", (int)mon.total_size - mon.free_size,
-           mon.used_pct,
-           mon.frag_pct,
+    printf("used: %6d (%3d %%), frag: %3d %%, biggest free: %6d\n",
+           (int)mon.total_size - mon.free_size, mon.used_pct, mon.frag_pct,
            (int)mon.free_biggest_size);
 }
 /**
@@ -41,6 +40,7 @@ int tick_thread(void *data)
         SDL_Delay(5);   /*Sleep for 5 millisecond*/
         lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
     }
+    return 0;
 }
 void hal_init(void)
 {
@@ -49,38 +49,42 @@ void hal_init(void)
 
     /*Create a display buffer*/
     static lv_disp_buf_t disp_buf1;
-    static lv_color_t buf1_1[480 * 10];
-    lv_disp_buf_init(&disp_buf1, buf1_1, NULL, 480 * 10);
+    static lv_color_t buf1_1[LV_HOR_RES_MAX * 120];
+    lv_disp_buf_init(&disp_buf1, buf1_1, NULL, LV_HOR_RES_MAX * 120);
 
     /*Create a display*/
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv); /*Basic initialization*/
     disp_drv.buffer = &disp_buf1;
-    disp_drv.flush_cb = monitor_flush; /*Used when `LV_VDB_SIZE != 0` in lv_conf.h (buffered drawing)*/
+    disp_drv.flush_cb = monitor_flush;
     lv_disp_drv_register(&disp_drv);
 
     /* Add the mouse as input device
-     * Use the 'mouse' driver which reads the PC's mouse*/
+   * Use the 'mouse' driver which reads the PC's mouse*/
     mouse_init();
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv); /*Basic initialization*/
     indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = mouse_read; /*This function will be called periodically (by the library) to get the mouse position and state*/
+
+    /*This function will be called periodically (by the library) to get the mouse position and state*/
+    indev_drv.read_cb = mouse_read;
     lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv);
 
     /*Set a cursor for the mouse*/
-    lv_obj_t *cursor_obj = lv_img_create(lv_disp_get_scr_act(NULL), NULL); /*Create an image object for the cursor */
-    lv_img_set_src(cursor_obj, &mouse_cursor_icon);                        /*Set the image source*/
-    lv_indev_set_cursor(mouse_indev, cursor_obj);                          /*Connect the image  object to the driver*/
+    LV_IMG_DECLARE(mouse_cursor_icon);                        /*Declare the image file.*/
+    lv_obj_t *cursor_obj = lv_img_create(lv_scr_act(), NULL); /*Create an image object for the cursor */
+    lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+    lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
 
     /* Tick init.
-     * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about how much time were elapsed
-     * Create an SDL thread to do this*/
+   * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about
+   * how much time were elapsed Create an SDL thread to do this*/
     SDL_CreateThread(tick_thread, "tick", NULL);
 
     /* Optional:
-     * Create a memory monitor task which prints the memory usage in periodically.*/
-    lv_task_create(memory_monitor, 3000, LV_TASK_PRIO_MID, NULL);
+   * Create a memory monitor task which prints the memory usage in
+   * periodically.*/
+    lv_task_create(memory_monitor, 5000, LV_TASK_PRIO_MID, NULL);
 }
 void taskLVGL(void *pragma)
 {
